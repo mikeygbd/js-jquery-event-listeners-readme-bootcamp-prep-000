141fe49460d48907c69d5c23ce625abc0db3801c
Michael Soares <mikey.soares@gmail.com>
2018-10-24 17:56:03 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,8 +1,27 @@
 //define functions here
 
-$(document).ready(function(){
-$('p').on("click", function getIt()) {}
-// call functions here 
-  alert('Hey!')
+function getIt(){
+  $('p').on("click")
+  return  alert('Hey!')
 }
+function frameIt() {
+  $('img').load(function(){
+    $(this).addClass("tasty")
+  })
+}
+function pressIt(){
+  $('input').keydown(function(event){
+    if(event.which === 71){
+      alert("you pressed G.")
+    }
+  })
+}
+function submitIt(){
+  $('form').on('submit', function(){
+      alert("Your form is going to be submitted now.")
+  })
+}
+$(document).ready(function(){
+// call functions here
+
 });
