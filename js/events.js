4a009e9eb11fc10ad81d574f27d6ac14f9f1ede4
Michael Soares <mikey.soares@gmail.com>
2018-10-24 17:52:34 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,8 +1,29 @@
 //define functions here
 
-$(document).ready(function(){
-$('p').on("click", function getIt()) {}
-// call functions here 
-  alert('Hey!')
+function getIt(){
+  $('p').on("click")
+  return  alert('Hey!')
 }
+function frameIt() {
+  $('img').load(function(){
+    $(this).addClass("tasty")
+  })
+}
+function pressIt(){
+  $('input').keydown(function(event){
+    if(event.which === 71){
+      alert("you pressed G.")
+    }
+  })
+}
+function submitIt(){
+  $('input').keydown(function(event){
+    if(event.which === 13){
+      alert("Your form is going to be submitted now.")
+    }
+  })
+}
+$(document).ready(function(){
+// call functions here
+submitIt()
 });
